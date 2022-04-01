720b7dfdbaa45617661405487e66fee4bc699831
Dinalyn Permejo <dinalyn@convrtx.com>
2022-04-01 17:39:13 +0800

    End User | Routes-CRUD - GOW-29

---
+++ src/route/entities/route.entity.ts
@@ -0,0 +1,75 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { DecimalNumber } from "aws-sdk/clients/glue";
+import { Validate } from "class-validator";
+import { EntityHelper } from "src/utils/entity-helper"
+import { IsExist } from "src/utils/validators/is-exists.validator";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export class Route extends EntityHelper {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ApiProperty({ example: 'cbcfa8b8-3a25-4adb-a9c6-e325f0d0f3ae' })
+    @Validate(IsExist, ['User', 'id'], {
+        message: 'User not Found',
+    })
+    @Column({
+        type: "uuid",
+        nullable: false
+    })
+    user_id?: string;
+    
+    @ApiProperty({ example: 'First On the List' })
+    @Column({
+        length: 50,
+        nullable: false    
+    })
+    route_name?: string;
+
+    @ApiProperty({ example: 32.4832})
+    @Column({
+        type: 'decimal',
+        precision: 8,
+        scale: 4,
+        default: 0,
+        nullable: false  
+    })
+    start_point_long?: number;
+    
+    @ApiProperty({ example: 12.4233})
+    @Column({
+        type: 'decimal',
+        precision: 8,
+        scale: 4,
+        default: 0,
+        nullable: false
+    })
+    start_point_lat?: DecimalNumber;
+
+    @ApiProperty({ example: 65.5234})
+    @Column({
+        type: 'decimal',
+        precision: 8,
+        scale: 4,
+        default: 0,
+        nullable: false 
+    })
+    stop_point_long?: DecimalNumber;
+    
+    @ApiProperty({ example: 12.4233})
+    @Column({
+        type: 'decimal',
+        precision: 8,
+        scale: 4,
+        default: 0,
+        nullable: false  
+    })
+    stop_point_lat?: DecimalNumber;
+    
+    @CreateDateColumn()
+    created_date: Date;
+
+    @UpdateDateColumn()
+    updated_date: Date;
+}
