948c2fab7379bdc4d783053722ac1803d64bcc42
Abdul Haseeb <80007828+Haseeb71@users.noreply.github.com>
2022-12-13 18:20:42 +0500

    comment

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -4,14 +4,14 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
 import { Comment } from './entities/comment.entity';
 import { DeepPartial } from 'src/common/types/deep-partial.type';
-import {RoleEnum} from "../roles/roles.enum";
-import {UserEntity} from "../users/user.entity";
+import { RoleEnum } from "../roles/roles.enum";
+import { UserEntity } from "../users/user.entity";
 
 @Injectable()
 export class CommentService extends TypeOrmCrudService<Comment> {
   constructor(
-      @InjectRepository(Comment)
-      private commentRepository: Repository<Comment>,
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
   ) {
     super(commentRepository);
   }
@@ -26,17 +26,17 @@ export class CommentService extends TypeOrmCrudService<Comment> {
       user: req,
       postfeed_id: dto.postfeed_id,
       message: dto.message,
-    }  
-    /*const user = await UserEntity.findOne({
-      id:req
-    });  
-*/
-   const comment =  await this.saveEntity(newComment);
-   //comment['user'] = user;
+    }
+    const user = await UserEntity.findOne({
+      id: req
+    });
+
+    const comment = await this.saveEntity(newComment);
+    comment['user'] = user;
 
     return {
       status: HttpStatus.OK,
-      data: comment,      
+      data: comment,
     }
 
   }
@@ -47,17 +47,17 @@ export class CommentService extends TypeOrmCrudService<Comment> {
         postfeed_id: id
       },
     });
-    if(!users){
-      return{
-        "errors" : [
+    if (!users) {
+      return {
+        "errors": [
           {
-            message : "All comments for Post-Feeds not fetched!",
-            status : HttpStatus.BAD_REQUEST,
+            message: "All comments for Post-Feeds not fetched!",
+            status: HttpStatus.BAD_REQUEST,
           }
         ]
       }
 
     }
-    return {data: users};
+    return { data: users };
   }
 }
