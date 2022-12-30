e4c2c3bd97a7fd7661eef4b1cf054aa46141694f
Nabeel Tariq <nabeeltariq261@gmail.com>
2022-12-30 16:29:07 +0500

    paginate responses

--- src/messenger/message/message.controller.ts
+++ src/messenger/message/message.controller.ts
@@ -1,6 +1,8 @@
 import {Controller, Get, Param, Request, UseGuards} from '@nestjs/common';
-import {ApiBearerAuth, ApiOperation, ApiTags} from '@nestjs/swagger';
+import { Query } from '@nestjs/common/decorators';
+import {ApiBearerAuth, ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
 import {Crud, CrudController, Override} from '@nestjsx/crud';
+import { query } from 'express';
 
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import {Message} from "./message.entity";
@@ -38,11 +40,12 @@ export class MessageController implements CrudController<Message> {
   get base(): CrudController<Message> {
     return this;
   }
-
+  
+  @ApiResponse({ type: Message })
   @ApiOperation({ summary: 'Get User Messages' })
   @Get('/:roomId')
-  async getUserMessages(@Param('roomId') roomId){
-    return await this.service.userMessages(roomId)
+  async getUserMessages(@Param('roomId') roomId, @Query() query){
+    return await this.service.userMessages(roomId, query.page)
   }
   @ApiOperation({ summary: 'Get Inbox' })
   @Get('/inbox')
