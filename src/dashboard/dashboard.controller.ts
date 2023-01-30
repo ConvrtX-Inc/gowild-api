d79cd7710f92248c5afb48b9fc3259522f0735a0
jawadsadiq78 <svl1g.superiior@gmail.com>
2023-01-30 16:27:11 +0500

    dashboard completed with download CSV feature

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Res} from '@nestjs/common';
 
 import { DashboardService } from './dashboard.service';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { UseGuards } from '@nestjs/common/decorators';
+import {Query, UseGuards} from '@nestjs/common/decorators';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import {NotFoundException} from "../exceptions/not-found.exception";
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
@@ -21,5 +22,29 @@ export class DashboardController {
   @Get('users-info')
   getUserCount() {
     return this.service.getUserCount();
+  }
+
+  @ApiOperation({ summary: 'Retrieve all Users' })
+  @Get('users')
+  async getAllUsers() {
+   return{ data: await this.service.getAllUserData() }
+  }
+
+  @ApiOperation({ summary: 'Download CSV file' })
+  @Get('csv')
+  async downloadCsv(@Res() res, @Query('created_date') createdDate) {
+    let data;
+    if (createdDate) {
+      data = await this.service.getUsersByCreatedDate(createdDate);
+      if (!data.length) {
+        throw new NotFoundException({ message: 'No users found with provided created date' })
+      }
+    } else {
+      data = await this.service.downloadUserData();
+    }
+      const csv =  await this.service.convertToCsv(data);
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+      res.send(csv);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {Between, Repository} from 'typeorm';
 import { FindOptions } from 'src/common/types/find-options.type';
 import { DeepPartial } from 'src/common/types/deep-partial.type';
 import { StatusEnum } from 'src/auth/status.enum';
@@ -206,26 +206,139 @@ export class UsersService extends TypeOrmCrudService<UserEntity> {
     return data;
   }
 
+  async findUnmappedUsers(){
+    const users = await this.usersRepository.find({
+      relations: ['role'],
+      where: {
+        role: { name: RoleEnum.USER }}
+    });
+
+    return users
+  }
+
   async getUserCount() {
     const allUsers = await this.usersRepository.find({});
     return {
-      signup_users: await this.usersRepository.count({}),
+      signup_users: await this.usersRepository.count({
+        relations: ['role'],
+        where: { role:{ name: RoleEnum.USER } }
+      }),
       active_users: await this.usersRepository.count({
-        relations: ['status'],
+        relations: ['status','role'],
         where: {
+          role: {
+            name: RoleEnum.USER
+          },
           status: {
             statusName: StatusEnum.Active,
           },
         },
       }),
       inactive_users: await this.usersRepository.count({
-        relations: ['status'],
+        relations: ['status', 'role'],
         where: {
+          role: {
+            name: RoleEnum.USER
+          },
           status: {
             statusName: StatusEnum.Inactive,
           },
         },
       }),
     };
+  }
+
+
+  async getDataByCreatedDate(createdDate: string) {
+    const startOfDay = new Date(createdDate);
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+
+    const users = await this.usersRepository.find({
+      relations:['role'],
+
+      where: {
+        role:{ name: RoleEnum.USER },
+        createdDate: Between(startOfDay, endOfDay)
+      }
+    });
+    if(!users){
+      throw new NotFoundException({message: "Users Not Found"})
+    }
+    const tenMinutesBefore = new Date();
+    tenMinutesBefore.setMinutes(tenMinutesBefore.getMinutes() - 10);
+    const data = users.map((obj) => {
+      const container: {
+        firstName: string;
+        lastName: string;
+        gender: string;
+        phoneNo: string;
+        picture: string;
+        email: string;
+        dateOfBirth: Date;
+        onlineStatus: boolean;
+        location_1: string;
+        location_2: string;
+        accountStatus: string;
+      } = {
+        firstName: obj.firstName,
+        lastName: obj.lastName,
+        gender: obj.gender,
+        phoneNo: obj.phoneNo,
+        picture: obj.picture,
+        email: obj.email,
+        dateOfBirth: obj.birthDate,
+        onlineStatus: obj.updatedDate > tenMinutesBefore,
+        location_1: obj.addressOne,
+        location_2: obj.addressTwo,
+        accountStatus: obj.status.statusName,
+      };
+      return container;
+    });
+    return data;
+  }
+
+  async downloadDashboardEntities() {
+
+    const users = await this.usersRepository.find({
+      relations: ['role'],
+      where: {
+        role: {
+          name: RoleEnum.USER,
+        },
+      },
+    });
+    const tenMinutesBefore = new Date();
+    tenMinutesBefore.setMinutes(tenMinutesBefore.getMinutes() - 10);
+    const data = users.map((obj) => {
+      const container: {
+        firstName: string;
+        lastName: string;
+        gender: string;
+        phoneNo: string;
+        picture: string;
+        email: string;
+        dateOfBirth: Date;
+        onlineStatus: boolean;
+        location_1: string;
+        location_2: string;
+        accountStatus: string;
+      } = {
+        firstName: obj.firstName,
+        lastName: obj.lastName,
+        gender: obj.gender,
+        phoneNo: obj.phoneNo,
+        picture: obj.picture,
+        email: obj.email,
+        dateOfBirth: obj.birthDate,
+        onlineStatus: obj.updatedDate > tenMinutesBefore,
+        location_1: obj.addressOne,
+        location_2: obj.addressTwo,
+        accountStatus: obj.status.statusName,
+      };
+      return container;
+    });
+    return data;
   }
 }
