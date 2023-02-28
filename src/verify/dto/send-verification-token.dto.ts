6c9f645272e5955043535ecb077e0c80d816c041
Waqar Yazdani <waqaryazdani2@gmail.com>
2023-02-28 19:06:45 +0500

    Changes

--- src/verify/dto/send-verification-token.dto.ts
+++ src/verify/dto/send-verification-token.dto.ts
@@ -4,11 +4,11 @@ import { IsPhoneNumber } from 'class-validator';
 
 export class SendVerificationTokenDto {
   @ApiProperty({ example: '+639506703405' })
-  // @Transform((value: string) =>
-  //   value.toLowerCase().trim().charAt(1) != '+'
-  //     ? '+' + value.toLowerCase().trim().charAt(1)
-  //     : value.toLowerCase().trim().charAt(1),
-  // )
+  @Transform((value: string) =>
+    value.toLowerCase().trim().charAt(1) != '+'
+      ? '+' + value.toLowerCase().trim().charAt(1)
+      : value.toLowerCase().trim().charAt(1),
+  )
   //@IsPhoneNumber()
-  emailPhone: string;
+  phone_number: string;
 }
