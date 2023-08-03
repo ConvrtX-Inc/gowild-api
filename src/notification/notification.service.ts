3691ee0c24ada5bf7cdb4abadbef05cc5508931d
Waqar Yazdani <waqaryazdani2@gmail.com>
2023-08-03 12:53:56 +0500

    Changes

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -35,12 +35,12 @@ export class NotificationService extends TypeOrmCrudService<Notification> {
       },
     });
     console.log('user',user)
-    if (user) {
-      console.log('In Notification')
-      console.log('FCM Token', user.fcm_token)
-      console.log('FCM Token', user.device_type)
-      await this.customPush(user.fcm_token, title, message, type, user.device_type)
-    }
+    // if (user) {
+    //   console.log('In Notification')
+    //   console.log('FCM Token', user.fcm_token)
+    //   console.log('FCM Token', user.device_type)
+    //   await this.customPush(user.fcm_token, title, message, type, user.device_type)
+    // }
 
     return {
       data: addNotification,
